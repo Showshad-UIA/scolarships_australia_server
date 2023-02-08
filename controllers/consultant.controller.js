06d8423820f5514bd58f51e03749f35a7bb3caaa
MD Masum Rana <masummx3@gmail.com>
2023-02-08 22:20:02 +0600

    working add consultant

--- controllers/consultant.controller.js
+++ controllers/consultant.controller.js
@@ -1,3 +1,6 @@
 exports.getConsultantInfo=async(req,res,next)=>{
     res.send("yes get consultant info")
 }
+exports.saveConsultantInfo=async(req,res,next)=>{
+    res.send("yes save consultant info")
+}
